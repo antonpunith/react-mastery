fe072eafc47af09ccb5147ec8f8a0599b88cc850
Anton Punith <anton@ozzi.tech>
2025-03-06 14:18:20 +1100

    add comments

--- src/sections/memo-demo.tsx
+++ src/sections/memo-demo.tsx
@@ -1,5 +1,6 @@
 import { useState, memo } from "react";
 
+// Main component that contains the state and renders the Greeting components
 const MemoDemo = () => {
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
@@ -13,7 +14,9 @@ const MemoDemo = () => {
                 Address{': '}
                 <input value={address} onChange={e => setAddress(e.target.value)} />
             </label>
+            {/* Regular Greeting component that will re-render whenever the parent component re-renders */}
             <Greeting name={name} isMemoized={false} />
+            {/* Memoized Greeting component that will only re-render if the `name` prop changes */}
             <MemoizedGreeting name={name} isMemoized={true} />
         </div>
     );
@@ -21,10 +24,12 @@ const MemoDemo = () => {
 
 export default MemoDemo;
 
+// Regular Greeting component that logs every time it is rendered
 const Greeting = ({ name, isMemoized }: { name: string, isMemoized: boolean }) => {
     console.log(`${isMemoized ? 'Memoized' : 'Non memoized'}  Greeting was rendered at`, new Date().toLocaleTimeString());
     return <h3>Hello{name && ', '}{name}!</h3>;
 }
 
+// Memoized version of the Greeting component using React's `memo` HOC
 const MemoizedGreeting = memo(Greeting)
 
