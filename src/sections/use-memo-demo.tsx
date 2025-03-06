89358b5a9ae0bc378616030187ae26127d56b36c
Anton Punith <anton@ozzi.tech>
2025-03-06 12:27:58 +1100

    add comments

--- src/sections/use-memo-demo.tsx
+++ src/sections/use-memo-demo.tsx
@@ -2,24 +2,27 @@ import { useEffect, useState, ChangeEvent, useMemo } from "react";
 import { getMovies, MovieData } from "../api";
 
 const useMemoDemo = () => {
-
+    // State to store list of movies
     const [moviesList, setMoviesList] = useState<MovieData[]>([]);
 
+    // State to store use input
     const [filterInput, setFilterInput] = useState<string>('')
 
-    useEffect(()=>{
+    // Fetch movies using the API when the component mounts
+    useEffect(() => {
         getMovies().then((data) => setMoviesList(data))
-    },[])
+    }, [])
+
+    // set filterInput when user types into the input field
     const onInput = (event: ChangeEvent<HTMLInputElement>) => {
         setFilterInput(event?.target?.value.toLowerCase().trim());
     }
-
-
+    // use Memo caches the filteredMovies result and recalculates it only when moviesList or filterInput changes
     const filteredMovies = useMemo(() => {
         return moviesList.filter((movie) => {
             return movie.name.toLowerCase().trim().includes(filterInput)
         })
-     }, [moviesList, filterInput ])
+    }, [moviesList, filterInput])
 
     return (
         <div className="container">
@@ -37,5 +40,7 @@ const useMemoDemo = () => {
         </div>
     );
 };
+
+// useMemo is a React Hook that lets you cache the result of a calculation between re-renders.
 
 export default useMemoDemo;
